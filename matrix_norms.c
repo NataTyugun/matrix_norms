a284ce9e06aad2cb0cb1fbd646147bd50ff13277
Anastasia Vorobyova <99526918+vvoroby@users.noreply.github.com>
2023-05-13 18:42:10 +0300

    Create matrix_norms.c

---
+++ matrix_norms.c
@@ -0,0 +1,76 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+
+/*норма бесконечность*/
+static PyObject *infinity_norm(PyObject *self, PyObject *args){
+    PyObject* list;
+    int i, j, n, m;
+
+    /* преобразуем тип данных */
+    /* PyArg_ParseTuple - обработка аргументов функции и трасформация типы данных Python в типы данных C.
+    Первый агрумент PyObject, кортеж переданных функции аргументов,  второй - тип переменных в которые мы хотим преобразовать наши аргументы
+    ("O" - произвольный агрумент, то есть будет сохраняться объект Python (без какого-либо преобразования) в указателе объекта C). Далее идут сами
+    переменныке в которые мы сохраняем преобразованные значения. Также осуществляется проверка, корректно ли отработала функция. */
+    if(!PyArg_ParseTuple(args,"O", &list)) {
+        return NULL; /* проверка */
+    }
+
+    n = PyList_GET_SIZE(list); /* количество строк */
+    m = PyList_GET_SIZE(PySequence_Fast_GET_ITEM(list, 0)); /* количество столбцов */
+
+    double sum = 0;
+    double max = 0;
+    double elem;
+
+    for (i=0; i<n; i++ ){
+        PyObject* tempi = PySequence_Fast_GET_ITEM(list, i); /* перебираем строки */
+        for ( j=0; j<m; j++ ){ /* перебираем элементы в строке */
+            PyObject* tempj = PySequence_Fast_GET_ITEM(tempi, j);
+            elem = PyFloat_AsDouble(tempj);
+            sum += elem;
+        }
+        if (max < sum){
+            max = sum;
+        }
+        sum = 0;
+    }
+    return PyFloat_FromDouble(max);
+}
+
+/* Описывает методы модуля */
+static PyMethodDef ownmod_methods[] = {
+    {
+    "infinity",          /* название функции внутри python*/
+     infinity_norm,        /* функция C */
+     METH_VARARGS,   /* макрос, поясняющий, что функция у нас с аргументами */
+     "infinity_norm_docs" /* документация для функции внутри python */
+    },
+
+    { NULL, NULL, 0, NULL } /*Сколько бы элементов структуры
+                            у вас не было, этот нулевой элемент должен быть всегда,
+                            и при этом быть последним*/
+};
+
+/* Описываем наш модуль */
+static PyModuleDef matrix_norms = {
+    PyModuleDef_HEAD_INIT,   /* обязательный макрос */
+    "matrix_norms",          /* my_plus.__name__*/
+    "infinity norm documentation", /* my_plus.__doc__ */
+    -1,
+    ownmod_methods  /* сюда передаем методы модуля */
+};
+
+/* в названии функции обязательно должен быть префикс PyInit */
+PyMODINIT_FUNC PyInit_matrix_norms(void) {
+      PyObject* module;
+      /* создаем модуль */
+      module = PyModule_Create(&matrix_norms);
+      /* если все корректно, то эта проверка не проходит */
+      if (module == NULL)
+          return NULL;
+      return module;
+}
