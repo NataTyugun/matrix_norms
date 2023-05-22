647a667295392f9c67fd50d6a810e19c0585945d
Anastasia Vorobyova <99526918+vvoroby@users.noreply.github.com>
2023-05-23 01:00:03 +0300

    Update matrix_norms.c

--- matrix_norms.c
+++ matrix_norms.c
@@ -99,6 +99,134 @@ static PyObject* euclidean_norm(PyObject *self, PyObject *args){
     return PyFloat_FromDouble(sqrt(sum));
 }
 
+/*обратная матрица*/
+static PyObject* inversion(PyObject* self, PyObject* args) {
+    PyObject *list;
+
+    if(!PyArg_ParseTuple(args,"O", &list)) {
+        return NULL;
+    }
+
+    int n, m;
+    n = PyList_GET_SIZE(list);
+    m = PyList_GET_SIZE(PySequence_Fast_GET_ITEM(list, 0));
+
+    /*проверка на квадратность*/
+    if (n != m) {
+        PyErr_SetString(PyExc_TypeError, "The matrix is not square");
+        return NULL;
+    }
+
+    const double EPS = 1E-9;
+    double det = 1;
+
+    PyObject* a = PyList_New(n);
+    long i,j,k;
+
+    for( i=0; i<n; i++ ) {
+        PyObject* row = PyList_GetItem(list, i);
+        PyObject* r = PyList_New(n);
+
+        for( j=0; j<n; j++ )
+        PyList_SetItem(r, j, PyList_GetItem(row, j));
+        PyList_SetItem(a, i, r);
+    }
+
+    PyObject* NEW_MATRIX = PyList_New(n);
+    for( i=0; i<n; i++ ) {
+        PyObject* r = PyList_New(n);
+        for( j=0; j<n; j++ )
+            if( i == j ) {
+                PyList_SetItem(r, j, PyFloat_FromDouble(1));
+            }
+            else {
+                PyList_SetItem(r, j, PyFloat_FromDouble(0));
+            }
+            PyList_SetItem(NEW_MATRIX, i, r);
+        }
+
+    double temp1, temp2, temp3;
+
+    for(i=0; i<n; i++) {
+        k = i;
+        for(j=i+1; j<n; j++) {
+            temp1 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, j), i) );
+            temp2 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, k), i) );
+            if (fabs(temp1) > fabs(temp2)) k = j;
+        }
+
+        temp1 = PyFloat_AsDouble(PyList_GetItem(PyList_GetItem(a, k), i));
+        if(fabs(temp1) < EPS) {
+            PyErr_SetString(PyExc_ValueError, "The discriminant is zero");
+            return NULL;
+        }
+
+        if(i != k) {
+            det = -det;
+            for(j=0; j<n; j++) {
+                temp1 = PyFloat_AsDouble(PyList_GetItem(PyList_GetItem(a, i), j));
+                temp2 = PyFloat_AsDouble(PyList_GetItem(PyList_GetItem(a, k), j));
+                PyList_SetItem(PyList_GetItem(a, i), j, PyFloat_FromDouble(temp2));
+                PyList_SetItem(PyList_GetItem(a, k), j, PyFloat_FromDouble(temp1));
+
+                temp1 = PyFloat_AsDouble(PyList_GetItem(PyList_GetItem(NEW_MATRIX, i), j));
+                temp2 = PyFloat_AsDouble(PyList_GetItem(PyList_GetItem(NEW_MATRIX, k), j));
+                PyList_SetItem(PyList_GetItem(NEW_MATRIX, i), j, PyFloat_FromDouble(temp2));
+                PyList_SetItem(PyList_GetItem(NEW_MATRIX, k), j, PyFloat_FromDouble(temp1));
+            }
+        }
+
+        temp1 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, i), i));
+        det *= temp1;
+
+        for(j=i; j<n; j++) {
+            temp2 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, i), j));
+            PyList_SetItem( PyList_GetItem(a, i), j, PyFloat_FromDouble(temp2/temp1));
+        }
+
+        for(j=0; j<n; j++) {
+            temp2 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(NEW_MATRIX, i), j));
+            PyList_SetItem( PyList_GetItem(NEW_MATRIX, i), j, PyFloat_FromDouble(temp2/temp1));
+        }
+
+        for( j=i+1; j<n; j++ ) {
+            temp1 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, j), i) );
+            if( fabs(temp1) > EPS ) {
+                for( k=0; k<n; k++ ) {
+                    temp2 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, i), k) );
+                    temp3 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, j), k) );
+                    PyList_SetItem( PyList_GetItem(a, j), k, PyFloat_FromDouble(temp3-temp2*temp1) );
+
+                    temp2 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(NEW_MATRIX, i), k) );
+                    temp3 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(NEW_MATRIX, j), k) );
+                    PyList_SetItem( PyList_GetItem(NEW_MATRIX, j), k, PyFloat_FromDouble(temp3-temp2*temp1) );
+                }
+            }
+        }
+    }
+
+    for(i = n-1; i > 0; i--) {
+        for(int k = 0; k < i; k++) {
+            temp1 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, k), i) );
+            if (fabs(temp1) > EPS) {
+                for (j = i-1; j < n; j++) {
+                    temp2 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, i), j) );
+                    temp3 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(a, k), j) );
+                    PyList_SetItem( PyList_GetItem(a, k), j, PyFloat_FromDouble(temp3-temp2*temp1) );
+                }
+
+                for (int j = 0; j < n; j++){
+                    temp2 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(NEW_MATRIX, i), j) );
+                    temp3 = PyFloat_AsDouble( PyList_GetItem(PyList_GetItem(NEW_MATRIX, k), j) );
+                    PyList_SetItem( PyList_GetItem(NEW_MATRIX, k), j, PyFloat_FromDouble(temp3-temp2*temp1) );
+                }
+            }
+        }
+    }
+
+   return NEW_MATRIX;
+}
+
 /*------------------------------------------------------------------------------------------*/
 
 /* Описывает методы модуля */
